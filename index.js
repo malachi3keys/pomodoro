79856671208640f1dfc7338686ff0890d98c0f55
malachi3keys <bhibbler.pltw@gmail.com>
2022-09-13 16:06:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var hourlist = []
+var minutelist = []
+var secondslist = []
+var a = 25
+var b = 60
+
+for (var i=0; i<a; i++){
+    hourlist.push(i)
+}
+
+for (var i=0; i<b; i++){
+    minutelist.push(i)
+    secondslist.push(i)
+}
+
+// console.log(hourlist)
+// console.log(minutelist)
+// console.log(secondslist)
+// document.querySelector("#hour")
