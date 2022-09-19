c2358c5bf05dc823fa11acafdc6e825850751df8
malachi3keys <bhibbler.pltw@gmail.com>
2022-09-19 17:31:14 -0400

    fix button behavior issues

--- index.js
+++ index.js
@@ -3,51 +3,58 @@ const pomodoro = 25;
 const shortBreak = 5;
 const longBreak = 15;
 
+//Time memory
+var addtime = 0;
+var holdtime = 0; //keep track of time left when paused
+var page = 'pomodoro';
+var timeinterval = null;
+
+//Identify divs that will need to be updated
+const minDiv = document.querySelector('#minutes');
+const secDiv = document.querySelector('#seconds');
+
 //Set clock for 1st pomodoro as default
 loadClock('pomodoro');
 
+//Event listeners for length of countdown
 document.querySelector('#p').addEventListener('click', () => loadClock('pomodoro'));
 document.querySelector('#b').addEventListener('click', () => loadClock('short'));
 document.querySelector('#j').addEventListener('click', () => loadClock('long'));
 
-//if hit pause, then save time left as variable
-//reset will update clock to 25 minutes
-//need buttons for break time or automatically go to break time after main clock goes down
+//Event listeners to start, pause, or reset the timer
+document.querySelector('#start').addEventListener('click', () => initializeClock(addtime));
+document.querySelector('#pause').addEventListener('click', pauseClock);
+document.querySelector('#reset').addEventListener('click', resetClock);
 
-function loadClock(type){
-    const clockDiv = document.querySelector('#time-div');
-    const min = clockDiv.querySelector('#minutes');
-    const sec = clockDiv.querySelector('#seconds');
-    var addtime = 0;
+function loadClock(type) {
+    // Stop any running timer when switching to another one 
+    if (timeinterval) {
+        clearClock();
+    }
 
-    switch (type){
+    //Update clock based on timer selection
+    switch (type) {
         case 'pomodoro':
-            min.innerHTML = ('0' + `${pomodoro}`).slice(-2);
+            minDiv.innerHTML = ('0' + `${pomodoro}`).slice(-2);
             addtime = pomodoro*60*1000;
+            page = type;
             break;
         case 'short':
-            min.innerHTML = ('0' + `${shortBreak}`).slice(-2);
-            addtime = shortBreak*60*1000;
-            break;
+            minDiv.innerHTML = ('0' + `${shortBreak}`).slice(-2);
+            addtime = shortBreak*60*1000; 
+            page = type;            break;
         case 'long':
-            min.innerHTML = ('0' + `${longBreak}`).slice(-2);
+            minDiv.innerHTML = ('0' + `${longBreak}`).slice(-2);
             addtime = longBreak*60*1000;
+            page = type;
             break;
         default:
-            min.innerHTML = `${pomodoro}`;
+            minDiv.innerHTML = `${pomodoro}`;
             addtime = pomodoro*60*1000;
+            page = 'pomodoro';
     }
-    
-    sec.innerHTML = '00';
-
-    // return{
-    //     addtime
-    // };
-
-    //Event listeners to start, pause, or reset the timer
-    document.querySelector('#start').addEventListener('click', () => initializeClock(addtime));
-    // document.querySelector('#pause').addEventListener('click', pause);
-    // document.querySelector('#reset').addEventListener('click', reset_timer);
+   
+    secDiv.innerHTML = '00';
 }
 
 function countdown(time) {
@@ -63,24 +70,43 @@ function countdown(time) {
 }
   
 function initializeClock(addtime) {
-    const clock = document.querySelector('#time-div');
-    const mdiv = clock.querySelector('#minutes');
-    const sdiv = clock.querySelector('#seconds');
+    //Prevent start of multiple countdowns
+    if (!timeinterval) {
+        //Determine the exact end time for the countdown
+        const time = new Date(Date.parse(new Date()) + addtime);
 
-    //Determine the exact end time for the countdown
-    const time = new Date(Date.parse(new Date()) + addtime);
+        function updateClock() {
+            const t = countdown(time);
+            holdtime = t.total;
 
-    function updateClock() {
-        const t = countdown(time);
+            minDiv.innerHTML = ('0' + t.minutes).slice(-2);
+            secDiv.innerHTML = ('0' + t.seconds).slice(-2);
 
-        mdiv.innerHTML = ('0' + t.minutes).slice(-2);
-        sdiv.innerHTML = ('0' + t.seconds).slice(-2);
-
-        if (t.total <= 0) {
-        clearInterval(timeinterval);
+            if (t.total <= 0) {
+                clearClock();
+            }
         }
-    }
 
-    updateClock();
-    const timeinterval = setInterval(updateClock, 1000);
+        updateClock();
+        timeinterval = setInterval(updateClock, 1000);
+    }     
+}
+
+function pauseClock() {
+    if (timeinterval) {
+        clearClock();
+        addtime = holdtime;    
+    }     
+}
+
+function resetClock() {
+    clearClock();
+
+    //Update clock based on current selected timer
+    loadClock(page);
+}
+
+function clearClock() {
+    clearInterval(timeinterval);
+    timeinterval = null;
 }
